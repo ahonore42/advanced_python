b3dfe7ff68cc0704d579339ab016eb80fa6b4f3e
ahonore42 <a1honore80@gmail.com>
2020-09-23 16:25:07 -0400

    [bracket_matcher] add small fixes to test cases

--- bracket_matcher.py
+++ bracket_matcher.py
@@ -11,35 +11,35 @@
 # Think you got it figured out? Run through the test cases below to make sure!
 
 
-def bracket_matcher(str):
+def bracket_matcher(stri):
   # code goes here
-  return 
+  return
 
 
 
 print(bracket_matcher('abc(123)'))
-# returns true
+# returns True
 
 print(bracket_matcher('a[b]c(123'))
-# returns false -- missing closing parens
+# returns False -- missing closing parens
 
 print(bracket_matcher('a[bc(123)]'))
-# returns true
+# returns True
 
 print(bracket_matcher('a[bc(12]3)'))
-# returns false -- improperly nested
+# returns False -- improperly nested
 
 print(bracket_matcher('a{b}{c(1[2]3)}'))
-# returns true
+# returns True
 
 print(bracket_matcher('a{b}{c(1}[2]3)'))
-# returns false -- improperly nested
+# returns False -- improperly nested
 
 print(bracket_matcher('()'))
-# returns true
+# returns True
 
 print(bracket_matcher('[]]'))
-# returns false - no opening bracket to correspond with last character
+# returns False - no opening bracket to correspond with last character
 
 print(bracket_matcher('abc123yay'))
-# returns true -- no brackets = correctly matched
+# returns True -- no brackets = correctly matched
